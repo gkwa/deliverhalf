ce59b0e9a2c57a5c4a20489807b5b7da07679239
Taylor Monacelli <taylormonacelli@gmail.com>
2023-05-22 13:50:48 -0700

    Add fixup workaround for install.ps1

--- install.ps1
+++ install.ps1
@@ -1,18 +1,54 @@
-function UpdateConfig(){
-    $tempDirectory = [System.IO.Path]::GetTempPath()
-    Set-Location $tempDirectory
-    & "C:\Program Files\Deliverhalf\deliverhalf.exe" config fetch
-
-    if (Test-Path -Path $tempDirectory\.deliverhalf.yaml) {
-        $oldConfig = "$programdataDirectory\.deliverhalf.$(Get-Date -f MM-dd-yyyy_HH_mm_ss).yaml"
-        Move-Item $programdataDirectory\.deliverhalf.yaml $oldConfig
-        Move-Item $tempDirectory\.deliverhalf.yaml $programdataDirectory\.deliverhalf.yaml
+function Uninstall(){
+    $s = Get-Service | Where-Object { $_.Name -eq 'deliverhalf' }
+    if ($s -and $s.Status -eq 'Running') {
+        Stop-Service deliverhalf -PassThru
+        Write-Host "Stopped deliverhalf service."
+    }
+    if ($s) {
+        sc.exe delete deliverhalf
+        Write-Host "Removed deliverhalf service."
     }
 
-    if (Test-Path -Path $programdataDirectory\.deliverhalf.yaml) {
-        $oldConfig = "$programdataDirectory\.deliverhalf.$(Get-Date -f MM-dd-yyyy_HH_mm_ss).yaml"
-        Move-Item $programdataDirectory\.deliverhalf.yaml $oldConfig
-        New-Item -Path $directoryPath -ItemType Directory | Out-Null
+    $paths = @(
+        "C:\Programdata\deliverhalf"
+        "C:\Program Files\Deliverhalf"
+        "$env:USERPROFILE\.deliverhalf.yaml"
+    )
+
+    Set-Location C:/Windows/Temp
+    Foreach ($path in $paths){
+        if (Test-Path -Path $path) {
+            Remove-Item -Force -Recurse $path
+            if (-not (Test-Path -Path $path)) {
+                Write-Host "$path has been removed"
+            } else {
+                Write-Host "could not remove $path"
+            }
+        }
+    }
+}
+
+function UpdateConfig() {
+    $path = "$programdataDirectory\deliverhalf\.deliverhalf.yaml"
+
+    if (Test-Path -Path $path) {
+        $oldConfig = "$programdataDirectory\deliverhalf\.deliverhalf.$(Get-Date -f MM-dd-yyyy_HH_mm_ss).yaml"
+        Move-Item $path $oldConfig
+    }
+
+    if (Test-Path -Path $env:USERPROFILE\.deliverhalf.yaml) {
+        $oldConfig = "$programdataDirectory\deliverhalf\.deliverhalf.$(Get-Date -f MM-dd-yyyy_HH_mm_ss).yaml"
+
+        if (Test-Path -Path $path) {
+            Move-Item $path $oldConfig
+        }
+    }
+
+    & "C:\Program Files\Deliverhalf\deliverhalf.exe" config fetch
+
+    if (Test-Path -Path $env:USERPROFILE\.deliverhalf.yaml) {
+        New-Item -Type "directory" -Force -Path $programdataDirectory\deliverhalf | Out-Null
+        Move-Item $env:USERPROFILE\.deliverhalf.yaml $programdataDirectory\deliverhalf\.deliverhalf.yaml
     }
 }
 
@@ -80,10 +116,78 @@ function InstallService() {
     & "$targetDirectory\nssm.exe" set deliverhalf Start SERVICE_AUTO_START
     & "$targetDirectory\nssm.exe" set deliverhalf DisplayName deliverhalf
     & "$targetDirectory\nssm.exe" set deliverhalf AppDirectory $programdataDirectory
-    & "$targetDirectory\nssm.exe" set deliverhalf AppParameters "client send forever --config '""$programdataDirectory\.deliverhalf.yaml""'"
+    & "$targetDirectory\nssm.exe" set deliverhalf AppParameters "client send forever --log-level trace --config '""$programdataDirectory\deliverhalf\.deliverhalf.yaml""'"
 
     Get-Service deliverhalf
     Start-Service deliverhalf -PassThru
+}
+
+function FindConfigs() {
+    $configs = @(
+        "$programdataDirectory\deliverhalf\.deliverhalf.yaml"
+        "$env:USERPROFILE\.deliverhalf.yaml"
+    )
+    foreach ($config in $configs){
+        if (Test-Path -Path $config) {
+            Write-Host "exists: $config"
+        } else {
+            Write-Host "gone: $config"
+        }
+    }
+}
+
+function fixup(){
+    $targetDirectory = "C:\Program Files\Deliverhalf"
+    $env:PATH += ";$targetDirectory"
+    StopService
+    FindConfigs
+    Set-Location C:\Programdata\deliverhalf
+    deliverhalf config fetch --log-level trace
+    FindConfigs
+    if (Test-Path -Path $env:USERPROFILE\.deliverhalf.yaml) {
+        Write-Host "removing $env:USERPROFILE\.deliverhalf.yaml"
+        Remove-Item $env:USERPROFILE\.deliverhalf.yaml
+    }
+    FindConfigs
+    deliverhalf client send --log-level trace
+    &"$targetDirectory\nssm.exe" set deliverhalf AppParameters "client send forever"
+    nssm get deliverhalf AppParameters
+    &"$targetDirectory\nssm.exe" set deliverhalf AppParameters client send forever --delay 1m --log-level trace --config '""C:\Users\Administrator\.deliverhalf.yaml""'
+    nssm get deliverhalf AppParameters
+    Start-Service deliverhalf
+    Start-Sleep -Seconds 2
+    Get-Content C:\Programdata\deliverhalf\deliverhalf.log -Tail 5
+    FindConfigs
+    StopService
+    deliverhalf config fetch --log-level trace
+    FindConfigs
+    Start-Service deliverhalf
+    Start-Sleep -Seconds 2
+    Get-Content C:\Programdata\deliverhalf\deliverhalf.log -Tail 5
+    Get-Service deliverhalf
+    FindConfigs
+
+    StopService
+    UpdateConfig
+    deliverhalf config fetch --log-level trace
+    FindConfigs
+    Start-Service deliverhalf
+    Start-Sleep -Seconds 2
+    Get-Content C:\Programdata\deliverhalf\deliverhalf.log -Tail 5
+    Get-Service deliverhalf
+    FindConfigs
+
+    StopService
+    $path = "$programdataDirectory\deliverhalf\.deliverhalf.yaml"
+    if (Test-Path -Path $path) {
+        $oldConfig = "$programdataDirectory\deliverhalf\.deliverhalf.$(Get-Date -f MM-dd-yyyy_HH_mm_ss).yaml"
+        Move-Item $path $oldConfig
+    }
+    deliverhalf config fetch --log-level trace
+    FindConfigs
+    Start-Service deliverhalf
+    Start-Sleep -Seconds 2
+    Get-Content C:\Programdata\deliverhalf\deliverhalf.log -Tail 5
 }
 
 function Main() {
@@ -94,6 +198,7 @@ function Main() {
     CheckAndDownloadFile -url $downloadUrl -filePath $zipFilePath -expiryMinutes 60
     ExtractZipFile -zipFilePath $zipFilePath -destinationPath $targetDirectory
 
+    StopService
     Write-Host "Deliverhalf executable extracted to: $exeFilePath"
 
     CheckAndDownloadFile -url $nssmDownloadUrl -filePath $nssmZipFilePath -expiryMinutes 60
@@ -102,6 +207,7 @@ function Main() {
     Get-ChildItem -Recurse -Path $programdataDirectory, $targetDirectory | Select-Object -ExpandProperty Fullname
     UpdateConfig
     InstallService
+    fixup
 }
 
 Main
